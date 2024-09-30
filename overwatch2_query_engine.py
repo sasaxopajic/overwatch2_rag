694a5f35b31ef1b68af128389ade0b5c8383c569
sasaxopajic <sasaxxxpajic@gmail.com>
2024-09-30 13:30:40 +0200

    Remove unused class OverwatchChatRouter

--- overwatch2_query_engine.py
+++ overwatch2_query_engine.py
@@ -18,6 +18,7 @@ def create_general_retriever_tool(index):
     retriever = index.as_retriever()
     return RetrieverTool.from_defaults(retriever, name="general_retriever", description="Useful for general information about Overwatch 2")
 
+# Function to create chat engine by combining general and all hero specific retrievers
 def create_chat_engine(index):
     tools = [create_hero_retriever_tool(index, hero) for hero in HEROES]
     tools.append(create_general_retriever_tool(index))
@@ -26,23 +27,3 @@ def create_chat_engine(index):
         streaming=True,
         verbose=True
     )
-
-# Router class for determining which retriever to use
-class OverwatchChatRouter:
-    def __init__(self, index):
-        self.index = index
-    
-    # Extract the hero name from the user's query
-    def extract_hero_name(self, query):
-        for hero in HEROES:
-            if hero.lower() in query.lower():
-                return hero
-        return None
-    
-    # Route the query to the appropriate retriever
-    def route_query(self, query):
-        hero_name = self.extract_hero_name(query)
-        if hero_name:
-            return create_hero_retriever_tool(self.index, hero_name)
-        else:
-            return create_general_retriever_tool(self.index)
